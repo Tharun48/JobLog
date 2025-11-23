8ad0db6c54c158480e77f07bfd4ec02696c2ad18
Tharun Reddy <tharunreddyy48@gmail.com>
2025-11-23 12:59:47 +0530

    intial commit

---
+++ backend/JobLog/src/main/java/com/joblog/JobLog/JobLogApplication.java
@@ -0,0 +1,13 @@
+package com.joblog.JobLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobLogApplication.class, args);
+	}
+
+}
---
+++ backend/JobLog/src/test/java/com/joblog/JobLog/JobLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joblog.JobLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
